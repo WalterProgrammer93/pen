725725dce7dafcc276557cacce1f3d1ab99709df
WalterDAW <co2full@outlook.es>
2020-11-18 12:04:33 +0100

    Modificacion del fichero database.php

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '192.168.10.10'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'pen'),
             'username' => env('DB_USERNAME', 'root'),
