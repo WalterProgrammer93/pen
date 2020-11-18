f2fca490fe364a6a4518d263041659a6937f6a62
WalterDAW <co2full@outlook.es>
2020-11-18 19:30:16 +0100

    Quitar el campo codigo en todas las tablas

--- database/seeds/Perfiles.php
+++ database/seeds/Perfiles.php
@@ -14,18 +14,17 @@ class Perfiles extends Seeder
     public function run()
     {
         //
-
         DB::table('perfils')->insert([
-        	['codigo' => '8A49F83J5F', 'rol' => 'Administrador'],
+        	['rol' => 'Administrador'],
         ]);
         DB::table('perfils')->insert([
-            ['codigo' => 'K302NH9RRL', 'rol' => 'Estudiante'],
+            ['rol' => 'Estudiante'],
         ]);
         DB::table('perfils')->insert([
-            ['codigo' => '2O3NF939D2', 'rol' => 'Profesor'],
+            ['rol' => 'Profesor'],
         ]);
         DB::table('perfils')->insert([
-            ['codigo' => '1L06H5BS40', 'rol' => 'Invitado'],
+            ['rol' => 'Invitado'],
         ]);
     }
 }
