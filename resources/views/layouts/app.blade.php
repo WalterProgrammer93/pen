7823975b65c9d4e310148c0405f2c8ddb23f24bd
WalterDAW <co2full@outlook.es>
2020-11-18 17:50:48 +0100

    Modificado el fichero de layout app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,7 +123,6 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('configuracion') }}">Configuracion</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
