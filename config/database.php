3c943c854c87bfa87040c2cd95938f8de115fd59
WalterDAW <co2full@outlook.es>
2020-11-18 11:55:15 +0100

    Configurando el fichero database.php del directorio config para la conexion con la base de datos del servidor.

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'pen'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'W2580*?+93@589314015585'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
