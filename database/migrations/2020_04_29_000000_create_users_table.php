858fe39a69c3166b0041565ebcf35ebb312e3a11
WalterDAW <co2full@outlook.es>
2020-11-18 11:18:56 +0100

    Creacion y modificacion del fichero de migraciones 2020_04_29_000000_create_users_table.php

--- database/migrations/2020_04_29_000000_create_users_table.php
+++ database/migrations/2020_04_29_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('user_id');
             $table->string('nombre',100);
             $table->string('email',100)->unique();
             $table->string('password');
