20dd90be7036d5e2bd33923bd9105c7d8688a0a4
WalterDAW <co2full@outlook.es>
2020-11-20 19:05:32 +0100

    Modificado los campos y los metodos del modelo Nota.php

--- app/Nota.php
+++ app/Nota.php
@@ -8,14 +8,14 @@ use pen\Asignatura;
 
 class Nota extends Model
 {
-    protected $primaryKey = 'id';
-    protected $fillable = ['codigo','eva1','eva2','eva3','media','alumno','asignatura'];
+    protected $primaryKey = 'nota_id';
+    protected $fillable = ['eva1','eva2','eva3','media','alumno','asignatura'];
 
     public function alumno() {
-    	return $this->hasMany(Alumno::class, 'id');
+    	return $this->hasMany(Alumno::class);
     }
 
     public function asignatura() {
-    	return $this->hasMany(Asignatura::class, 'id');
+    	return $this->hasMany(Asignatura::class);
     }
 }
