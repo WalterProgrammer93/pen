d9e133fc83dadcc52223a294f4ed077c658469c5
WalterDAW <co2full@outlook.es>
2020-11-20 18:37:55 +0100

    creacion de la tabla relacional entre perfils y users, y añadido las claves foraneas

---
+++ database/migrations/2020_11_20_162355_create_perfils_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerfilsUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfils_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('perfil_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('perfil_id')->references('perfil_id')->on('perfils');
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfils_user');
+    }
+}
