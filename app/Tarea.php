4294292844f83831dcfcc357d15e083b1edc0229
WalterDAW <co2full@outlook.es>
2020-11-20 19:08:30 +0100

    Modificado los campos y los metodos del modelo Tarea.php

--- app/Tarea.php
+++ app/Tarea.php
@@ -8,13 +8,13 @@ use pen\Asignatura;
 
 class Tarea extends Model
 {
-    protected $primaryKey = 'id';
+    protected $primaryKey = 'tarea_id';
     protected $fillable = ['codigo','titulo','descripcion','autor','fecha_envio','fecha_entrega','hora_entrega','calificacion','asignatura','tema'];
 
     public function asignatura() {
-    	return $this->hasMany(Asignatura::class, 'id');
+    	return $this->hasMany(Asignatura::class);
     }
     public function tema() {
-    	return $this->hasMany(Tema::class, 'id');
+    	return $this->hasMany(Tema::class);
     }
 }
