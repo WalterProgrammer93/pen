878b66b179ef3ce57278baabe8f46c20ae24b1e4
WalterDAW <co2full@outlook.es>
2020-11-20 18:36:24 +0100

    quitar la foreign key de la tabla users

--- database/migrations/2020_04_29_000000_create_users_table.php
+++ database/migrations/2020_04_29_000000_create_users_table.php
@@ -18,9 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('nombre',100);
             $table->string('email',100)->unique();
             $table->string('password');
-            //$table->string('rol',100)->nullable();
-            $table->integer('perfil_id')->unsigned()->nullable();
-            $table->foreign('perfil_id')->references('perfil_id')->on('perfils');
             $table->timestamps();
         });
 
