dcd2e75a8bfb0a5597a8cf8eb98a03d73f98ca4a
WalterDAW <co2full@outlook.es>
2020-11-20 17:01:30 +0100

    Creacion de la funcion perfil para relacionar los objetos User y Perfil para la base de datos.

--- app/User.php
+++ app/User.php
@@ -39,6 +39,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];*/
 
+    public function perfils() {
+      return $this->belongsTo(Perfil::class);
+    }
+
     public function administrador() {
 
         if ($this->perfil->rol == 'Administrador') {
