b510777df19c57aef94393945e2855e08fa8ee81
WalterDAW <co2full@outlook.es>
2020-11-20 19:09:01 +0100

    Modificado los campos y los metodos del modelo User.php

--- app/Tema.php
+++ app/Tema.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tema extends Model
 {
-    protected $primaryKey = 'id';
-    protected $fillable = ['codigo','nombre', 'titulo', 'documento_tema','documento_tarea'];
-    
+    protected $primaryKey = 'tema_id';
+    protected $fillable = ['nombre', 'titulo', 'documento_tema','documento_tarea'];
+
 }
