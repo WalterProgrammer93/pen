47a9b5ed9019c8662d6db491a36834c8955ffcfd
WalterDAW <co2full@outlook.es>
2020-11-21 20:41:39 +0100

    Modificado la primary key del modelo Tarea.php

--- app/Tarea.php
+++ app/Tarea.php
@@ -8,7 +8,7 @@ use pen\Asignatura;
 
 class Tarea extends Model
 {
-    protected $primaryKey = 'tarea_id';
+    protected $primaryKey = 'id';
     protected $fillable = ['codigo','titulo','descripcion','autor','fecha_envio','fecha_entrega','hora_entrega','calificacion','asignatura','tema'];
 
     public function asignatura() {
