a68ad63afcbbb3ef6d1871ee184f6c3a456a297d
WalterDAW <co2full@outlook.es>
2020-11-21 20:13:01 +0100

    Modificado el campo user_id por id en la base de datos en la migracion

--- database/migrations/2020_04_29_000000_create_users_table.php
+++ database/migrations/2020_04_29_000000_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('user_id');
+            $table->increments('id');
             $table->string('nombre',100);
             $table->string('email',100)->unique();
             $table->string('password');
