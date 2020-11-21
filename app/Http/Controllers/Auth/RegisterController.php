6b608dd30cf3995b4a144710cb12dace4ed2ccc0
WalterDAW <co2full@outlook.es>
2020-11-21 21:03:07 +0100

    Modificado el codigo del metodo create para anexar el perfil al usuario del RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        $user->perfils()->attach(Perfil::where('perfil', 'user'));
+        $user->perfils()->attach(Perfil::where('perfil', 'user')->first());
         return $user;
     }
 }
