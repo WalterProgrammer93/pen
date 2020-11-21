ee0b2f4b968279c6f64ea8547a1f23562f34a475
WalterDAW <co2full@outlook.es>
2020-11-21 20:42:18 +0100

    Modificado la primary key del modelo Tema.php

--- app/Tema.php
+++ app/Tema.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tema extends Model
 {
-    protected $primaryKey = 'tema_id';
+    protected $primaryKey = 'id';
     protected $fillable = ['nombre', 'titulo', 'documento_tema','documento_tarea'];
 
 }
