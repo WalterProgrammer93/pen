68af3faa379bd4f8b17e701b5de228ff85ed9236
WalterDAW <co2full@outlook.es>
2020-11-23 20:18:14 +0100

    Modificado la vista del fichero index.blade.php de eventos

---
+++ resources/views/eventos/index.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('eventos') }}">Eventos</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Crear</li>
+            </ol>
+          </nav>
+          <div class="card">
+              <div class="card-header">Crear Curso</div>
+                <div class="card-body">
+                    @if (Session('status'))
+                        <div class="alert alert-success" role="alert">
+                              {{ session('status') }}
+                          </div>
+                      @endif
+                      @if (!empty($departamento->id))
+                          <div class="form-group row">
+                              <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                              <div class="col-md-6">
+                                  <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ $evento->nombre }}" required autocomplete="nombre">
+                                  @error('nombre')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion</label>
+                              <div class="col-md-6">
+                                  <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ $evento->descripcion }}" rows="4" cols="50" required autocomplete="descripcion"></textarea>
+                                  @error('descripcion')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label for="disponibilidad" class="col-md-4 col-form-label text-md-right">Disponibilidad</label>
+                              <div class="col-md-6">
+                                  <select id="disponibilidad" class="form-control" name="disponibilidad" required>
+                                      <option value="{{ $evento->disponibilidad }}">{{ $evento->disponibilidad }}</option>
+                                      <option value="No Disponible">No Disponible</option>
+                                  </select>
+                                  @error('disponibilidad')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="form-group row mb-0">
+                              <div class="col-md-6 offset-md-4">
+                                  <button type="submit" class="btn btn-success">
+                                      Actualizar
+                                  </button>
+                                  <button type="submit" class="btn btn-primary">
+                                      <a href="{{ url('eventos') }}" class="enlaceback">Cancelar</a>
+                                  </button>
+                              </div>
+                          </div>
+                      @else
+                          <div class="form-group row">
+                              <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                              <div class="col-md-6">
+                                  <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre">
+                                  @error('nombre')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion</label>
+                              <div class="col-md-6">
+                                  <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ old('descripcion') }}" rows="4" cols="50" required autocomplete="descripcion"></textarea>
+                                  @error('descripcion')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label for="disponibilidad" class="col-md-4 col-form-label text-md-right">Disponibilidad</label>
+                              <div class="col-md-6">
+                                  <select id="disponibilidad" class="form-control" name="disponibilidad" required>
+                                      <option value="">Seleccione una disponibilidad</option>
+                                      <option value="Disponible">Disponible</option>
+                                      <option value="No Disponible">No Disponible</option>
+                                  </select>
+                                  @error('disponibilidad')
+                                      <span class="invalid-feedback" role="alert">
+                                          <strong>{{ $message }}</strong>
+                                      </span>
+                                  @enderror
+                              </div>
+                          </div>
+                      @endif
+                      <form  method="POST" action="{{ route('eventos') }}">
+                          <div class="form-group row mb-0">
+                              <div class="col-md-6 offset-md-4">
+                                  <button type="submit" class="btn btn-success">Añadir</button>
+                                  <a href="{{ route('cursos') }}" class="btn btn-primary">Cancelar</a>
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+</div>
+@endsection
