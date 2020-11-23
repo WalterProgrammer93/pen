254711d19e89579cbea8c4fd9aeeb4604192ce35
WalterDAW <co2full@outlook.es>
2020-11-23 19:52:37 +0100

    Modificado la vista del formulario de crear.blade.php de departamentos

--- resources/views/departamentos/crear.blade.php
+++ resources/views/departamentos/crear.blade.php
@@ -4,98 +4,30 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('departamentos') }}">Departamentos</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Crear</li>
+              </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Crear Departamento</div>
-
                 <div class="card-body">
-                    @if (session('status'))
+                    <!--@if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
-
-                    <form method="POST" action="{{ url('altaDepartamento') }}" >
-                        
+                    @endif-->
+                    <!-- Obtengo la sesión actual del usuario -->
+                    {{ $message=Session::get('message') }}
+                    <!-- Muestro el mensaje de validación -->
+                    @include('alerts.request')
+                    <form method="POST" action="{{ route('departamentos/store') }}" >
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
-
-                            <div class="col-md-6">
-                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ old('codigo') }}" required autocomplete="codigo" autofocus>
-
-                                @error('codigo')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
-
-                            <div class="col-md-6">
-                                <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
-
-                                @error('nombre')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion</label>
-
-                            <div class="col-md-6">
-                                <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ old('descripcion') }}" rows="4" cols="50" required autocomplete="descripcion"></textarea>
-
-                                @error('descripcion')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="estado" class="col-md-4 col-form-label text-md-right">Estado</label>
-
-                            <div class="col-md-6">
-                                <select id="estado" class="form-control" name="estado" required>
-                                    <option value="Abierto">Abierto</option>
-                                    <option value="Cerrado">Cerrado</option>
-                                </select>
-
-                                @error('estado')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        
-                        <form method="POST" action="{{ url('departamentos') }}">
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-
-                                    <button type="submit" class="btn btn-success">
-                                        Añadir
-                                    </button>
-
-                                    <button type="submit" class="btn btn-primary">
-
-                                        <a href="{{ url('departamentos') }}" class="enlaceback">Cancelar</a>
-
-                                    </button>
-                          
-                                </div>             
-                            </div>
-                        </form>
+                        <input type="hidden" name="_method" value="PUT">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        @include('departamentos.index')
                     </form>
                 </div>
             </div>
