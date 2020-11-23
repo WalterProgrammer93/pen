0991b700a974e2e2f16a5b70476cab94061d9f9d
WalterDAW <co2full@outlook.es>
2020-11-23 14:39:08 +0100

    Modificado el nombre del fichero a crear.blade.php

---
+++ resources/views/clases/crear.blade.php
@@ -0,0 +1,110 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Crear Clase</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ url('altaClase', $asignaturas, $profesores) }}" >
+                        
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
+
+                            <div class="col-md-6">
+                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ old('codigo') }}" required autocomplete="codigo" autofocus>
+
+                                @error('codigo')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="asignatura" class="col-md-4 col-form-label text-md-right">Asignatura</label>
+
+                            <div class="col-md-6">
+                                <select id="asignatura" class="form-control" name="asignatura" required>
+                                    <option value="">Seleccione un asignatura</option>
+                                    @foreach($asignaturas as $nombre => $id)
+                                        <option value="{{ $nombre }}">{{ $id }}</option>
+                                    @endforeach
+                                </select>
+
+                                @error('asignatura')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="profesor" class="col-md-4 col-form-label text-md-right">Profesor</label>
+
+                            <div class="col-md-6">
+                                <select id="profesor" class="form-control" name="profesor" required>
+                                    <option value="">Seleccione un profesor</option>
+                                    @foreach($profesores as $nombre => $id)
+                                        <option value="{{ $nombre }}">{{ $id }}</option>
+                                    @endforeach
+                                </select>
+
+                                @error('profesor')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="horario" class="col-md-4 col-form-label text-md-right">Horario</label>
+
+                            <div class="col-md-6">
+                                <textarea id="horario" type="text" class="form-control @error('horario') is-invalid @enderror" name="horario" value="{{ old('horario') }}" required autocomplete="horario" autofocus></textarea>
+
+                                @error('horario')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+
+                            </div>
+                        </div>
+                        
+                        <form method="POST" action="{{ url('clases') }}">
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+
+                                    <button type="submit" class="btn btn-success">
+                                        Añadir
+                                    </button>
+
+                                    <button type="submit" class="btn btn-primary">
+                                        <a href="{{ url('clases') }}" class="enlaceback">Cancelar</a>
+                                    </button>
+                                </div>             
+                            </div>
+                        </form>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
