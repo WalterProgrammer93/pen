a4a5728410cb9c937eff2f2616eda07a8261d2ee
WalterDAW <co2full@outlook.es>
2020-11-23 13:33:02 +0100

    Modificado el metodo index en el fichero HomeController.php para la autorizacion y control de usuarios mediante perfiles o roles

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index(Request $request)
     {
-      $request->user()->authorizeRoles(['admin', 'user']);
+      $request->user()->authorizeRoles(['admin', 'user', 'student', 'profesor']);
       return view('home');
     }
 }
