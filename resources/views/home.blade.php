581a672edf982b39a5c9f0c49eb3e6c3d9dc844b
WalterDAW <co2full@outlook.es>
2020-11-23 12:56:07 +0100

    Añadiendo javaScript al home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,24 +30,19 @@
                           <p><a href="{{ url('eventos')}}" class="eventos"><img src="https://img.icons8.com/material/30/000000/scroll.png" alt="eventos">&nbsp; Eventos</a></p>
                           <p><a href="{{ url('asistencias')}}" class="asistencias"><img src="https://img.icons8.com/material/30/000000/scroll.png" alt="asistencias">&nbsp; Asistencias</a></p>
                           <p><a href="{{ url('perfiles')}}" class="eventos"><img src="https://img.icons8.com/material/30/000000/user-male-circle--v1.png"alt="usuario">&nbsp; Perfiles</a></p>
+                      @elseif (Auth::user()->hasRole('student'))
+                          <p><a href="{{ url('cursos') }}" class="cursos"><img src="https://img.icons8.com/ios-glyphs/30/000000/graduation-cap--v1.png" alt="cursos">&nbsp; Cursos</a></p>
+                          <p><a href="{{ url('asignaturas') }}" class="asignaturas"><img src="https://img.icons8.com/ios-glyphs/30/000000/courses.png" alt="asignaturas">&nbsp; Asignaturas</a></p>
+                          <p><a href="{{ url('notas') }}" class="notas"><img src="https://img.icons8.com/ios-filled/30/000000/report-card.png" alt="notas">&nbsp; Notas</a></p>
+                          <p><a href="{{ url('tareas') }}" class="tareas"><img src="https://img.icons8.com/ios-filled/30/000000/classroom.png" alt="tareas">&nbsp; Tareas</a></p>
+                          <p><a href="{{ url('temas') }}" class="temas"><img src="https://img.icons8.com/ios-glyphs/30/000000/courses.png" alt="temas">&nbsp; Temas</a></p>
                       @else
-                          <div>Usuario no autorizado</div>
-                      @endif
-
-                      @if(Auth::user()->hasRole('student'))
-                        <p><a href="{{ url('cursos') }}" class="cursos"><img src="https://img.icons8.com/ios-glyphs/30/000000/graduation-cap--v1.png" alt="cursos">&nbsp; Cursos</a></p>
-                        <p><a href="{{ url('asignaturas') }}" class="asignaturas"><img src="https://img.icons8.com/ios-glyphs/30/000000/courses.png" alt="asignaturas">&nbsp; Asignaturas</a></p>
-                        <p><a href="{{ url('notas') }}" class="notas"><img src="https://img.icons8.com/ios-filled/30/000000/report-card.png" alt="notas">&nbsp; Notas</a></p>
-                        <p><a href="{{ url('tareas') }}" class="tareas"><img src="https://img.icons8.com/ios-filled/30/000000/classroom.png" alt="tareas">&nbsp; Tareas</a></p>
-                        <p><a href="{{ url('temas') }}" class="temas"><img src="https://img.icons8.com/ios-glyphs/30/000000/courses.png" alt="temas">&nbsp; Temas</a></p>
-                      @else
-                          <div>Usuario no autorizado</div>
-                      @endif
+                          <script type="text/javascript" src="{{ asset('js/alerts.js') }}" defer></script>
+                          <div id="noAutorizado"></div>
                     @else
-                        <div>No estas logueado</div>
+                        <script type="text/javascript" src="{{ asset('jas/aler')}}" defer></script>
+                        <div id="noLogin"></div>
                     @endif
-
-
                 </div>
             </div>
         </div>
