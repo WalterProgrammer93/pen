b1c380f48d099d168d4e736c738ea3bb67cc6ed0
WalterDAW <co2full@outlook.es>
2020-11-23 16:18:10 +0100

    Modificado el codigo del fichero aulas.blade.php de aulas

--- resources/views/aulas/aulas.blade.php
+++ resources/views/aulas/aulas.blade.php
@@ -31,41 +31,31 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <table class="table table-striped">
-
+                    <table class="table table-striped table-bordered table-hover">
                         <tr>
-                            <th>Codigo</th>
                             <th>Etiqueta</th>
                             <th>Descripcion</th>
                             <th>Disponibilidad</th>
                             <th colspan="2">Acción</th>
-
                         </tr>
-
                         @foreach($aulas as $aula)
-
                             <tr>
-                                <td>{{ $aula->codigo }}</td>
-                                <td>{{ $aula->etiqueta }}</td>
-                                <td>{{ $aula->descripcion }}</td>
-                                <td>{{ $aula->disponibilidad }}</td>
-                                <td><a href="{{action('AulaController@edit', $aula['id'])}}" class="btn btn-success">Modificar</a></td>
-                                <td>
-                                    <form  onsubmit="return confirm('Do you really want to delete?');" action="{{action('AulaController@destroy', $aula->id)}}" method="post">
-                                        {{csrf_field()}}
-                                        <input name="_method" type="hidden" value="DELETE">
-                                        <button class="btn btn-danger" type="submit">Eliminar</button>
-                                     </form>
+                                <td class="v-align-middle">{{ $aula->etiqueta }}</td>
+                                <td class="v-align-middle">{{ $aula->descripcion }}</td>
+                                <td class="v-align-middle">{{ $aula->disponibilidad }}</td>
+                                <td class="v-align-middle">
+                                  <form action="{{ route('aulas/eliminar', $aula->id) }}" method="POST" class="form-horizontal" role="form" onsubmit="return confirmarEliminar()">
+                                      <input type="hidden" name="_method" value="PUT">
+                                      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                      <a href="{{ route('aulas/actualizar', $aula->id) }}" class="btn btn-primary">Modificar</a>
+                                      <button type="submit" class="btn btn-danger">Eliminar</button>
+                                  </form>
                                 </td>
                             </tr>
                         @endforeach
                     </table>
-
-                    <form action="{{ url('añadirAula') }}" method="POST">
-
+                    <form action="{{ route('aulas/crear') }}" method="POST">
                         @csrf
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-0">
                                 <button type="submit" class="btn btn-success">
@@ -74,7 +64,7 @@
                                 <button type="submit" class="btn btn-primary">
                                     <a href="{{ url('home') }}" class="enlaceback">Volver a menu</a>
                                 </button>
-                            </div> 
+                            </div>
                         </div>
                     </form>
                 </div>
