9cdd96b53c2c72cddb33de89da8683c915272a7d
WalterDAW <co2full@outlook.es>
2020-11-23 17:08:03 +0100

    Modificado el codigo del formulario del fichero index.blade.php de asignaturas

--- resources/views/asignaturas/index.blade.php
+++ resources/views/asignaturas/index.blade.php
@@ -4,58 +4,52 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('asignaturas') }}">Asignturas</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Crear</li>
+            </ol>
+          </nav>
           <div class="card">
               <div class="card-header">Crear Asignatura</div>
-
                 <div class="card-body">
                     @if (Session('status'))
                         <div class="alert alert-success" role="alert">
                               {{ session('status') }}
                           </div>
                       @endif
-
-                      @if ( !empty ($asignatura->id) )
-
+                      @if (!empty($asignatura->id))
                         <div class="form-group row">
                             <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
-
                             <div class="col-md-6">
                                 <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ $asignatura->nombre }}" required autocomplete="nombre" autofocus>
-
                                 @error('nombre')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion</label>
-
                             <div class="col-md-6">
                                 <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ $asignatura->descripcion }}" rows="4" cols="50" required autocomplete="descripcion"/>
-
                                 @error('descripcion')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
-
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="curso" class="col-md-4 col-form-label text-md-right">Curso</label>
-
                             <div class="col-md-6">
                                 <select id="curso" class="form-control" name="curso" required>
                                     @foreach($asignaturas as $asignatura)
                                         <option value="{{ $asignatura->curso }}">{{ $asignatura->curso }}</option>
                                     @endforeach
                                 </select>
-
                                 @error('curso')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -63,10 +57,8 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="aula" class="col-md-4 col-form-label text-md-right">Aula</label>
-
                             <div class="col-md-6">
                                 <select id="aula" class="form-control" name="aula" required>
                                     <option value="">Seleccione una aula</option>
@@ -74,7 +66,6 @@
                                         <option value="{{ $asignatura->aula }}">{{ $asignatura->aula }}</option>
                                     @endforeach
                                 </select>
-
                                 @error('curso')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -82,42 +73,31 @@
                                 @enderror
                             </div>
                         </div>
-
                       @else
-
                           <div class="form-group row">
                               <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
-
                               <div class="col-md-6">
                                   <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
-
                                   @error('nombre')
                                       <span class="invalid-feedback" role="alert">
                                           <strong>{{ $message }}</strong>
                                       </span>
                                   @enderror
-
                               </div>
                           </div>
-
                           <div class="form-group row">
                               <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion</label>
-
                               <div class="col-md-6">
                                   <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ old('descripcion') }}" rows="4" cols="50" required autocomplete="descripcion"></textarea>
-
                                   @error('descripcion')
                                       <span class="invalid-feedback" role="alert">
                                           <strong>{{ $message }}</strong>
                                       </span>
                                   @enderror
-
                               </div>
                           </div>
-
                           <div class="form-group row">
                               <label for="curso" class="col-md-4 col-form-label text-md-right">Curso</label>
-
                               <div class="col-md-6">
                                   <select id="curso" class="form-control" name="curso" required>
                                       <option value="">Seleccione un curso</option>
@@ -125,7 +105,6 @@
                                           <option value="{{ $nombre }}">{{ $id }}</option>
                                       @endforeach
                                   </select>
-
                                   @error('curso')
                                       <span class="invalid-feedback" role="alert">
                                           <strong>{{ $message }}</strong>
@@ -133,10 +112,8 @@
                                   @enderror
                               </div>
                           </div>
-
                           <div class="form-group row">
                               <label for="aula" class="col-md-4 col-form-label text-md-right">Aula</label>
-
                               <div class="col-md-6">
                                   <select id="aula" class="form-control" name="aula" required>
                                       <option value="">Seleccione una aula</option>
@@ -144,7 +121,6 @@
                                           <option value="{{ $etiqueta }}">{{ $id }}</option>
                                       @endforeach
                                   </select>
-
                                   @error('curso')
                                       <span class="invalid-feedback" role="alert">
                                           <strong>{{ $message }}</strong>
@@ -153,13 +129,14 @@
                               </div>
                           </div>
                       @endif
-
-                      <div class="form-group row mb-0">
-                          <div class="col-md-6 offset-md-4">
-                              <button type="submit" class="btn btn-success">Añadir</button>
-                              <a href="{{ route('asignaturas') }}" class="btn btn-primary">Cancelar</a>
-                          </div>
-                      </div>
+                      <form method="POST" action="{{ route('asignaturas') }}">
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-success">Añadir</button>
+                                <a href="{{ route('asignaturas') }}" class="btn btn-primary">Cancelar</a>
+                            </div>
+                        </div>
+                      </form>
                   </div>
               </div>
           </div>
