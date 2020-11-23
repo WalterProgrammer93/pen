0538af633bb3f84ad5b02e109063c6e1ddc0ef33
WalterDAW <co2full@outlook.es>
2020-11-23 19:49:27 +0100

    Modificado el codigo del formulario de la vista crear.blade.php de convalidaciones

--- resources/views/convalidaciones/crear.blade.php
+++ resources/views/convalidaciones/crear.blade.php
@@ -4,13 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
-              <li class="breadcrumb-item"><a href="{{ url('convalidaciones') }}">Convalidaciones</a></li>
-              <li class="breadcrumb-item active" aria-current="page">Crear</li>
-            </ol>
-          </nav>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('convalidaciones') }}">Convalidaciones</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Crear</li>
+              </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Crear Convalidacion</div>
                 <div class="card-body">
