dd534fc33d4dbe5a3b9d58a27e214e4e41f1c7f9
WalterDAW <co2full@outlook.es>
2020-11-23 16:49:35 +0100

    Modificado el codigo del formulario del fichero editar.blade.php

--- resources/views/aulas/editar.blade.php
+++ resources/views/aulas/editar.blade.php
@@ -22,99 +22,30 @@
     </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('aulas') }}">Aulas</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Editar</li>
+            </ol>
+          </nav>
             <div class="card">
                 <div class="card-header">Modificar Aula</div>
-
                 <div class="card-body">
+                  <!--@if (session('status'))
+                      <div class="alert alert-success" role="alert">
+                          {{ session('status') }}
+                      </div>
+                  @endif-->
 
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                  <!-- Obtengo la sesión actual del usuario -->
+                  {{ $message=Session::get('message') }}
 
-
-                    <form method="POST" action="{{ action('AulaController@update', $id) }}">
-
+                  <!-- Muestro el mensaje de validación -->
+                  @include('alerts.request')
+                    <form method="POST" action="{{ route('aulas/editar', $aula->id) }}">
                         {{csrf_field()}}
-
                         <input type="hidden" name="_method" value="PATCH">
-
-                        <div class="form-group row">
-                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
-
-                            <div class="col-md-6">
-                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ $aula->codigo }}" required autocomplete="codigo" autofocus>
-
-                                @error('codigo')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="etiqueta" class="col-md-4 col-form-label text-md-right">Etiqueta</label>
-
-                            <div class="col-md-6">
-                                <input id="etiqueta" type="text" class="form-control @error('etiqueta') is-invalid @enderror" name="etiqueta" value="{{ $aula->etiqueta }}" required autocomplete="etiqueta" autofocus>
-
-                                @error('etiqueta')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="descripcion" class="col-md-4 col-form-label text-md-right">Descripcion</label>
-
-                            <div class="col-md-6">
-                                <textarea id="descripcion" class="form-control @error('descripcion') is-invalid @enderror" name="descripcion" value="{{ $aula->descripcion }}" rows="4" cols="50" autocomplete="descripcion"></textarea>
-
-                                @error('descripcion')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="disponibilidad" class="col-md-4 col-form-label text-md-right">Disponibilidad</label>
-
-                            <div class="col-md-6">
-                                <select id="disponibilidad" class="form-control" name="disponibilidad" required>
-                                    <option value="{{ $aula->disponibilidad }}">{{ $aula->disponibilidad }}</option>
-                                    <option value="No Disponible">No Disponible</option>
-                                </select>
-
-                                @error('disponibilidad')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-
-                                <button type="submit" class="btn btn-success">
-                                    Actualizar
-                                </button>
-                                
-                                <button type="submit" class="btn btn-primary">
-
-                                    <a href="{{ url('aulas') }}" class="enlaceback">Cancelar</a>
-
-                                </button>
-                            </div>             
-                        </div>
                     </form>
                 </div>
             </div>
