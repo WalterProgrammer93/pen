de1b57714438bb5320591b77c7f3ebe83bf6000d
WalterDAW <co2full@outlook.es>
2020-11-23 14:17:56 +0100

    Modificado el fichero editar.blade.php

--- resources/views/asistencias/edit.blade.php
+++ resources/views/asistencias/edit.blade.php
@@ -31,103 +31,16 @@
           </nav>
             <div class="card">
                 <div class="card-header">Modificar Asistencia</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     <form method="POST" action="{{ action('AsistenciaController@update', $alumnos, $cursos, $asignaturas) }}">
-
                         {{csrf_field()}}
-
                         <input type="hidden" name="_method" value="PATCH">
-
-                        <div class="form-group row">
-                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
-
-                            <div class="col-md-6">
-                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ $asistencia->codigo }}" required autocomplete="codigo" autofocus>
-
-                                @error('codigo')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="numero_horas" class="col-md-4 col-form-label text-md-right">Numero Horas</label>
-
-                            <div class="col-md-6">
-                                <input id="numero_horas" type="text" class="form-control @error('numero_horas') is-invalid @enderror" name="numero_horas" value="{{ $asistencia->numero_horas }}" required autocomplete="numero_horas" autofocus>
-
-                                @error('numero_horas')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="alumno" class="col-md-4 col-form-label text-md-right">Alumno</label>
-
-                            <div class="col-md-6">
-                                <select id="alumno" class="form-control" name="alumno" required>
-                                    @foreach($asistencias as $asistencia)
-                                        <option value="{{ $asistencia->alumno }}">{{ $asistencia->alumno }}</option>
-                                    @endforeach
-                                </select>
-
-                                @error('alumno')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="asignatura" class="col-md-4 col-form-label text-md-right">Asignatura</label>
-
-                            <div class="col-md-6">
-
-                                <select id="asignatura" class="form-control" name="asignatura" required>
-                                    @foreach($asistencias as $asistencia)
-                                        <option value="{{ $asistencia->asignatura }}">{{ $asistencia->asignatura }}</option>
-                                    @endforeach
-                                </select>
-
-                                @error('asignatura')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-
-                                <button type="submit" class="btn btn-success">
-                                    Actualizar
-                                </button>
-
-                                <button type="submit" class="btn btn-primary">
-
-                                    <a href="{{ url('asignaturas') }}" class="enlaceback">Cancelar</a>
-
-                                </button>
-                            </div>
-                        </div>
+                        @include(asistencias.index)
                     </form>
                 </div>
             </div>
