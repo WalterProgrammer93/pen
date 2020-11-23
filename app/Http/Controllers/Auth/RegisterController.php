546faa9015cfc5b9986e1f899c7c7ffd1e2b8b72
WalterDAW <co2full@outlook.es>
2020-11-23 17:26:26 +0100

    Modificado el codigo de la function create del controlador RegisterController.php para probar la autenticacion y autorizacion de usuario al home

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-        $user->perfils()->attach(Perfil::where('perfil', 'user')->first());
+        $user->perfils()->attach(Perfil::where('perfil', 'admin')->first());
         return $user;
     }
 }
