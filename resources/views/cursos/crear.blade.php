1cb26dd816bbf85dad2a27c39111c73670b83dc5
WalterDAW <co2full@outlook.es>
2020-11-23 19:50:28 +0100

    Modificado la vista de formulario de crear.blade.php de cursos

--- resources/views/cursos/crear.blade.php
+++ resources/views/cursos/crear.blade.php
@@ -4,6 +4,13 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('cursos') }}">Cursos</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Crear</li>
+              </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Crear Curso</div>
                 <div class="card-body">
