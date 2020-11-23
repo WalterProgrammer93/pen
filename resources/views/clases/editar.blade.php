c52f618e9a8ed885b1558e49b9808590d44f89c8
WalterDAW <co2full@outlook.es>
2020-11-23 18:11:52 +0100

    Modificado la vista del formulario del fichero editar.blade.php

--- resources/views/clases/editar.blade.php
+++ resources/views/clases/editar.blade.php
@@ -22,101 +22,30 @@
     </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ url('clases') }}">Clases</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Editar</li>
+            </ol>
+          </nav>
             <div class="card">
                 <div class="card-header">Modificar Clase</div>
-
                 <div class="card-body">
-                    @if (session('status'))
+                    <!--@if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                    @endif-->
 
-                    <form method="POST" action="{{ url('ClaseController@update', $id) }}" >
-                        
-                        @csrf
+                    <!-- Obtengo la sesión actual del usuario -->
+                    {{ $message=Session::get('message') }}
 
-                        <div class="form-group row">
-                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
-
-                            <div class="col-md-6">
-                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ clase->codigo }}" required autocomplete="codigo" autofocus>
-
-                                @error('codigo')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="asignatura" class="col-md-4 col-form-label text-md-right">Asignatura</label>
-
-                            <div class="col-md-6">
-                                <select id="asignatura" class="form-control" name="asignatura" required>
-                                    @foreach($clases as $clase)
-                                        <option value="{{ $clase->id }}">{{ $clase->id }}</option>
-                                    @endforeach
-                                </select>
-
-                                @error('asignatura')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="profesor" class="col-md-4 col-form-label text-md-right">Profesor</label>
-
-                            <div class="col-md-6">
-                                <select id="profesor" class="form-control" name="profesor" required>
-                                    @foreach($clases as $clase)
-                                        <option value="{{ $clase->id }}">{{ $clase->id }}</option>
-                                    @endforeach
-                                </select>
-
-                                @error('profesor')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="horario" class="col-md-4 col-form-label text-md-right">Horario</label>
-
-                            <div class="col-md-6">
-                                <textarea id="horario" type="text" class="form-control @error('horario') is-invalid @enderror" name="horario" value="{{ clase->horario }}" required autocomplete="horario" autofocus></textarea>
-
-                                @error('horario')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-                        
-                        <form method="POST" action="{{ url('clases') }}">
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-
-                                    <button type="submit" class="btn btn-success">
-                                        Añadir
-                                    </button>
-
-                                    <button type="submit" class="btn btn-primary">
-                                        <a href="{{ url('convalidaciones') }}" class="enlaceback">Cancelar</a>
-                                    </button>
-                                </div>             
-                            </div>
-                        </form>
+                    <!-- Muestro el mensaje de validación -->
+                    @include('alerts.request')
+                    <form method="POST" action="{{ route('clases/editar', $clase->id, $asignaturas, $profesores) }}" >
+                        {{csrf_field()}}
+                        <input type="hidden" name="_method" value="PATCH">
                     </form>
                 </div>
             </div>
