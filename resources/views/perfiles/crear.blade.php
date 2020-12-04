e8ef60d29ba23c5afb475e31dc7ebc4ecc7bd6b7
WalterDAW <co2full@outlook.es>
2020-12-04 19:23:33 +0100

    Modificado el fichero de crear.blade.php de perfiles

--- resources/views/perfiles/crear.blade.php
+++ resources/views/perfiles/crear.blade.php
@@ -4,55 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('perfiles') }}">Perfiles</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Crear</li>
+              </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Crear perfil</div>
-
                 <div class="card-body">
-                    <form method="POST" action="{{ url('altaPerfil') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
-
-                            <div class="col-md-6">
-                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ old('codigo') }}" required autocomplete="codigo" autofocus>
-
-                                @error('codigo')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="rol" class="col-md-4 col-form-label text-md-right">Rol</label>
-
-                            <div class="col-md-6">
-                                <input id="rol" type="text" class="form-control @error('rol') is-invalid @enderror" name="rol" value="{{ old('rol') }}" required autocomplete="rol" autofocus>
-
-                                @error('rol')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <form method="POST" action="{{ url('perfiles') }}">
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-success">
-                                        Añadir
-                                    </button>
-                                    <button type="submit" class="btn btn-primary">
-
-                                        <a href="{{ url('perfiles') }}" class="enlaceback">Cancelar</a>
-
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                    <!-- Obtengo la sesión actual del usuario -->
+                    {{ $message=Session::get('message') }}
+                    <!-- Muestro el mensaje de validación -->
+                    @include('alerts.request')
+                    <form method="POST" action="{{ route('perfiles/store') }}">
+                        <input type="hidden" method="_method" value="PUT">
+                        <input type="hidden" method="_method" value="{{ csrf_token() }}">
+                        @include('perfiles.index')
                     </form>
                 </div>
             </div>
