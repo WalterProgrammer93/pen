da93c7eced2a6e57f9b713e6dd9066eb3c9b6805
WalterDAW <co2full@outlook.es>
2020-12-04 18:17:33 +0100

    modificado el codigo del fichero crear.blade.php de eventos

--- resources/views/eventos/crear.blade.php
+++ resources/views/eventos/crear.blade.php
@@ -14,17 +14,11 @@
             <div class="card">
                 <div class="card-header">Crear Evento</div>
                 <div class="card-body">
-                    <!--@if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif-->
                     <!-- Obtengo la sesión actual del usuario -->
                     {{ $message=Session::get('message') }}
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
                     <form method="POST" action="{{ route('eventos/store') }}" >
-                        @csrf
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         @include('eventos.index')
