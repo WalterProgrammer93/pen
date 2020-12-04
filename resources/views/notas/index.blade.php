ba4ae137a67feeee7062df6035b7bc09dc12e6d4
WalterDAW <co2full@outlook.es>
2020-12-04 18:38:53 +0100

    Modificado el fichero index de notas

--- resources/views/notas/index.blade.php
+++ resources/views/notas/index.blade.php
@@ -181,7 +181,7 @@
                       <div class="form-group row mb-0">
                           <div class="col-md-6 offset-md-4">
                               <button type="submit" class="btn btn-success">Añadir</button>
-                              <a href="{{ route('cursos') }}" class="btn btn-primary">Cancelar</a>
+                              <a href="{{ route('notas') }}" class="btn btn-primary">Cancelar</a>
                           </div>
                       </div>
                   </div>
