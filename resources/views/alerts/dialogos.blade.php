224916361e933bc2b3efe16c74dc26615a067b48
WalterDAW <co2full@outlook.es>
2020-12-11 13:27:15 +0100

    añadir dialogo de confirmacion de eliminacion de registros

---
+++ resources/views/alerts/dialogos.blade.php
@@ -0,0 +1,23 @@
+<div class="modal" id="myModal">
+    <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-content">
+
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h1 class="modal-title">Eliminar</h1>
+          <button type="button" class="close" data-dismiss="modal">×</button>
+        </div>
+
+        <!-- Modal body -->
+        <div class="modal-body">
+          <p>¿Seguro que quieres eliminarlo?</p>
+        </div>
+
+        <!-- Modal footer -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        </div>
+
+      </div>
+    </div>
+  </div>
