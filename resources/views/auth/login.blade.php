933c15638451dbfec9309e6bd7478466c2308a06
WalterDAW <co2full@outlook.es>
2020-12-11 13:29:01 +0100

    Modificar el aspecto del login de la app

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,20 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header text-size-10">Iniciar Sesion</div>
+                <div class="row justify-content-center"><image src="imagenes/logo.png" alt="PEN Software"></div>
                 <div class="card-body">
-                    <div class="row justify-content-center"><image src="imagenes/logo.png" alt="PEN Software"></div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -24,13 +20,10 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
-
                             <div class="col-md-6">
                                 <input id="password" type="password" placeholder="Contraseña" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -38,12 +31,10 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
                                     <label class="form-check-label" for="remember">
                                         Recordar datos
                                     </label>
@@ -55,18 +46,14 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-success">
                                     Entrar
                                 </button>
                                 <button type="submit" class="btn btn-primary">
-
                                     <a href="{{ route('register') }}" class="enlaceback">Registrate</a>
-
                                 </button>
-                                
                             </div>
                         </div>
                     </form>
