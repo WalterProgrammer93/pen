414c41245405e31c53f923729c2f0ffcecc6baef
WalterDAW <co2full@outlook.es>
2020-12-13 17:57:05 +0100

    modificado la vista crear de usuarios

--- resources/views/usuarios/crear.blade.php
+++ resources/views/usuarios/crear.blade.php
@@ -4,91 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('usuarios') }}">Usuarios</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Crear</li>
+              </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Crear usuario</div>
                 <div class="card-body">
-
-                    <form method="POST" action="{{ action('UsuarioController@store', $roles) }}">
-
-                        {{csrf_field()}}
-
-                        <div class="form-group row">
-                            <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
-
-                            <div class="col-md-6">
-                                <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" required autocomplete="nombre" autofocus>
-
-                                @error('nombre')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="rol" class="col-md-4 col-form-label text-md-right">Rol</label>
-
-                            <div class="col-md-6">
-
-                              <select id="rol" class="form-control" name="rol" required>
-                                  <option value="">Asignar un Rol</option>
-                                  @foreach($roles as $rol => $id)
-                                      <option value="{{ $rol }}" @if($rol === $id) selected='selected' @endif)>{{ $id }}</option>
-                                  @endforeach
-                              </select>
-
-                                @error('rol')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <form method="POST" action="{{ url('usuarios') }}">
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-success">
-                                        Añadir
-                                    </button>
-                                    <button type="submit" class="btn btn-primary">
-
-                                        <a href="{{ url('usuarios') }}" class="enlaceback">Cancelar</a>
-
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                    <!-- Obtengo la sesión actual del usuario -->
+                    {{ $message=Session::get('message') }}
+                    <!-- Muestro el mensaje de validación -->
+                    @include('alerts.request')
+                    <form method="POST" action="{{ route('usuarios/store') }}">
+                        <input type="hidden" name="_method" value="PUT">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        @include('usuarios.index')
                     </form>
                 </div>
             </div>
