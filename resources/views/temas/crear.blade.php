db20bbefb361145e543e737db674ba3d4bf8cf55
WalterDAW <co2full@outlook.es>
2020-12-13 17:21:44 +0100

    modificar la vista crear temas

--- resources/views/temas/crear.blade.php
+++ resources/views/temas/crear.blade.php
@@ -4,96 +4,24 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ url('home') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ url('temas') }}">Temas</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Crear</li>
+              </ol>
+            </nav>
             <div class="card">
                 <div class="card-header">Crear Tema</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ url('altaTema') }}" enctype="multipart/form-data">
-                        
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="codigo" class="col-md-4 col-form-label text-md-right">Codigo</label>
-
-                            <div class="col-md-6">
-                                <input id="codigo" type="text" class="form-control @error('codigo') is-invalid @enderror" name="codigo" value="{{ old('codigo') }}" required autocomplete="codigo" autofocus>
-
-                                @error('codigo')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="contenido" class="col-md-4 col-form-label text-md-right">Contenido</label>
-
-                            <div class="col-md-6">
-                                <textarea id="contenido" class="form-control @error('contenido') is-invalid @enderror" name="contenido" value="{{ old('contenido') }}" rows="4" cols="50" required autocomplete="contenido"></textarea> 
-                                @error('contenido')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="documento_tema" class="col-md-4 col-form-label text-md-right">Documento Tema</label>
-
-                            <div class="col-md-6">
-                                <input id="documento_tema" type="file" class="form-control @error('documento_tema') is-invalid @enderror" name="documento_tema" value="Subir Tema" required>
-
-                                <a href="href='/descargarTema/{{$file}}" class="btn-success">Descargar Tema</a>
-
-                                @error('documento_tema')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="documento_tarea" class="col-md-4 col-form-label text-md-right">Documento Tarea</label>
-
-                            <div class="col-md-6">
-                                <input id="documento_tarea" type="file" class="form-control" name="documento_tarea" value="Subir Tarea" required>
-
-                                <a href="href='/descargarTarea/{{$file}}" class="btn-success">Descargar Tarea</a>
-
-                                @error('documento_tarea')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        
-                        <form method="POST" action="{{ url('temas') }}">
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-
-                                    <button type="submit" class="btn btn-success">
-                                        Añadir
-                                    </button>
-
-                                    <button type="submit" class="btn btn-primary">
-
-                                        <a href="{{ url('temas') }}" class="enlaceback">Cancelar</a>
-
-                                    </button>
-                          
-                                </div>             
-                            </div>
-                        </form>
+                    <!-- Obtengo la sesión actual del usuario -->
+                    {{ $message=Session::get('message') }}
+                    <!-- Muestro el mensaje de validación -->
+                    @include('alerts.request')
+                    <form method="POST" action="{{ route('temas/store') }}" enctype="multipart/form-data">
+                        <input type="hidden" method="_method" value="PUT">
+                        <input type="hidden" method="_token" value="{{ csrf_token() }}">
+                        @include('temas.index')
                     </form>
                 </div>
             </div>
