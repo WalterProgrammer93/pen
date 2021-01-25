48ec08c79bcf38a043a93837d80ed00393323adc
WalterDAW <co2full@outlook.es>
2021-01-25 18:01:39 +0100

    Modificado el codigo del formulario crear cursos

--- resources/views/cursos/crear.blade.php
+++ resources/views/cursos/crear.blade.php
@@ -15,10 +15,14 @@
                 <div class="card-header">Crear Curso</div>
                 <div class="card-body">
                     <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('cursos/store') }}" role="form">
+                    <form method="POST" action="{{ route('cursos/guardar') }}" role="form">
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         @include('cursos.index')
