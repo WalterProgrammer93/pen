20f2b2241343e2f3a658ac84aeb2dcaa9ffee682
WalterDAW <co2full@outlook.es>
2021-01-25 18:02:19 +0100

    Modificado el codigo del formulario de editar de cursos

--- resources/views/cursos/editar.blade.php
+++ resources/views/cursos/editar.blade.php
@@ -33,10 +33,14 @@
                 <div class="card-header">Editar Curso</div>
                 <div class="card-body">
                     <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('cursos/update', $cursos->id) }}" role="form">
+                    <form method="POST" action="{{ route('cursos/actualizar', $cursos->id) }}" role="form">
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         @include('cursos.index')
