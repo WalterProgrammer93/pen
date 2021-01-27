112d66b04fa4a98a1d08c3b5ebf5ae8de001005e
WalterDAW <co2full@outlook.es>
2021-01-27 18:24:15 +0100

    Modificado la vista de editar aulas

--- resources/views/aulas/editar.blade.php
+++ resources/views/aulas/editar.blade.php
@@ -32,20 +32,18 @@
             <div class="card">
                 <div class="card-header">Modificar Aula</div>
                 <div class="card-body">
-                  <!--@if (session('status'))
+                  <!-- Obtengo la sesión actual del usuario -->
+                  @if (session('status'))
                       <div class="alert alert-success" role="alert">
                           {{ session('status') }}
                       </div>
-                  @endif-->
-
-                  <!-- Obtengo la sesión actual del usuario -->
-                  {{ $message=Session::get('message') }}
-
+                  @endif
                   <!-- Muestro el mensaje de validación -->
                   @include('alerts.request')
-                    <form method="POST" action="{{ route('aulas/editar', $aula->id) }}">
-                        {{csrf_field()}}
-                        <input type="hidden" name="_method" value="PATCH">
+                    <form method="POST" action="{{ route('aulas/actualizar', $aula->id) }}">
+                      <input type="hidden" name="_method" value="PUT">
+                      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                      @include('aulas.index')
                     </form>
                 </div>
             </div>
