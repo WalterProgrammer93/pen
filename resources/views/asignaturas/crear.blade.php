16b5bc192ffd4118b6b6f9d2da6ada4dcc358b92
WalterDAW <co2full@outlook.es>
2021-01-27 17:49:31 +0100

    Modificado el formulario de crear asignaturas

--- resources/views/asignaturas/crear.blade.php
+++ resources/views/asignaturas/crear.blade.php
@@ -14,18 +14,14 @@
             <div class="card">
                 <div class="card-header">Crear Asignatura</div>
                 <div class="card-body">
-                    <!--@if (session('status'))
+                    @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif-->
-
-                    <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
-
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('asignaturas/store') }}" role="form">
+                    <form method="POST" action="{{ route('asignaturas/guardar') }}" role="form">
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         @include('asignaturas.index')
