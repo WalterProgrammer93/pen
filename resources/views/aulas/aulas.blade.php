11a4641cc04302709e7db15aadb2892842bf8ceb
WalterDAW <co2full@outlook.es>
2021-02-03 19:26:06 +0100

    Quitando espacios

--- resources/views/aulas/aulas.blade.php
+++ resources/views/aulas/aulas.blade.php
@@ -73,7 +73,6 @@
                                             @endif
                                           @endif
                                       @endif
-
                                   </form>
                                 </td>
                             </tr>
