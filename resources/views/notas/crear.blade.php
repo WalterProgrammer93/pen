1a93d057ff3d47940cdbf92a0c628ce0212989c7
WalterDAW <co2full@outlook.es>
2021-02-04 18:25:35 +0100

    Modificado la vista de crear notas

--- resources/views/notas/crear.blade.php
+++ resources/views/notas/crear.blade.php
@@ -15,10 +15,14 @@
                 <div class="card-header">Crear Nota</div>
                 <div class="card-body">
                     <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('notas/store', $alumno->id, $asignatura->id) }}" >
+                    <form method="POST" action="{{ route('notas/guardar') }}" >
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         @include('notas.index')
