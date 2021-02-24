e48c330887e291176c18d7e071115ab75c1c60a5
WalterDAW <co2full@outlook.es>
2021-02-24 18:05:58 +0100

    Modificado la vista de crear tarea

--- resources/views/tareas/crear.blade.php
+++ resources/views/tareas/crear.blade.php
@@ -15,10 +15,14 @@
                 <div class="card-header">Crear Tarea</div>
                 <div class="card-body">
                     <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('tareas/store', $asignatura->id, $tema->id) }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('tareas/guardar') }}" enctype="multipart/form-data" role="form">
                         <input type="hidden" method="_method" value="PUT">
                         <input type="hidden" method="_token" value="{{ csrf_token() }}">
                         @include('tareas.index')
