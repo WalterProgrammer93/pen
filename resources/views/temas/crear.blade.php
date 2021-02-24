55e85387e6324f9b32cbed7db5fd4cec14feb98b
WalterDAW <co2full@outlook.es>
2021-02-24 18:07:51 +0100

    Modificado la vista de crear temas

--- resources/views/temas/crear.blade.php
+++ resources/views/temas/crear.blade.php
@@ -15,10 +15,14 @@
                 <div class="card-header">Crear Tema</div>
                 <div class="card-body">
                     <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('temas/store') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('temas/guardar') }}" enctype="multipart/form-data" role="form">
                         <input type="hidden" method="_method" value="PUT">
                         <input type="hidden" method="_token" value="{{ csrf_token() }}">
                         @include('temas.index')
