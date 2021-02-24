b6daa1d06c5428e8a86ffbd768389550fec47ef0
WalterDAW <co2full@outlook.es>
2021-02-24 18:08:45 +0100

    Modificado la vista del formulario de temas

--- resources/views/temas/index.blade.php
+++ resources/views/temas/index.blade.php
@@ -19,103 +19,103 @@
                               {{ session('status') }}
                           </div>
                       @endif
-                      @if (!empty($tema->id))
-                          <div class="form-group row">
-                              <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
-                              <div class="col-md-6">
-                                  <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ $tema->nombre }}" required autocomplete="nombre" autofocus>
-                                  @error('nombre')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <div class="form-group row">
-                              <label for="contenido" class="col-md-4 col-form-label text-md-right">Contenido</label>
-                              <div class="col-md-6">
-                                  <textarea id="contenido" class="form-control @error('contenido') is-invalid @enderror" name="contenido" value="{{ $tema->contenido }}" rows="4" cols="50" required autocomplete="contenido"></textarea>
-                                  @error('contenido')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <div class="form-group row">
-                              <label for="documento_tema" class="col-md-4 col-form-label text-md-right">Documento Tema</label>
-                              <div class="col-md-6">
-                                  <input id="documento_tema" type="file" class="form-control @error('documento_tema') is-invalid @enderror btn-success" name="documento_tema" value="{{ $tema->documento_tema }}" required>
-                                  @error('documento_tema')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <div class="form-group row">
-                              <label for="documento_tarea" class="col-md-4 col-form-label text-md-right">Documento Tarea</label>
-                              <div class="col-md-6">
-                                  <input id="documento_tarea" type="file" class="form-control @error('documento_tarea') is-invalid @enderror btn-success" name="documento_tarea" value="{{ $tema->documento_tarea }}" required>
-                                  @error('documento_tarea')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                      @else
-                          <div class="form-group row">
-                              <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
-                              <div class="col-md-6">
-                                  <textarea id="nombre" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" rows="4" cols="50" required autocomplete="contenido"></textarea>
-                                  @error('nombre')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <div class="form-group row">
-                              <label for="contenido" class="col-md-4 col-form-label text-md-right">Contenido</label>
-                              <div class="col-md-6">
-                                  <textarea id="contenido" class="form-control @error('contenido') is-invalid @enderror" name="contenido" value="{{ old('contenido') }}" rows="4" cols="50" required autocomplete="contenido"></textarea>
-                                  @error('contenido')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <div class="form-group row">
-                              <label for="documento_tema" class="col-md-4 col-form-label text-md-right">Documento Tema</label>
-                              <div class="col-md-6">
-                                  <input id="documento_tema" type="file" class="form-control @error('documento_tema') is-invalid @enderror" name="documento_tema" value="Subir Tema" required>
-                                  @error('documento_tema')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
-                          <div class="form-group row">
-                              <label for="documento_tarea" class="col-md-4 col-form-label text-md-right">Documento Tarea</label>
-                              <div class="col-md-6">
-                                  <input id="documento_tarea" type="file" class="form-control" name="documento_tarea" value="Subir Tarea" required>
-                                  @error('documento_tarea')
-                                      <span class="invalid-feedback" role="alert">
-                                          <strong>{{ $message }}</strong>
-                                      </span>
-                                  @enderror
-                              </div>
-                          </div>
+                      @if(Auth::check())
+                        @if (!empty($temas->id))
+                          <form method="POST" action="{{ route('temas/actualizar', $temas->id) }}">
+                            @csrf
+                            <input type="hidden" name="_method" value="PUT">
+                            <div class="form-group row">
+                                <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                                <div class="col-md-6">
+                                    <input id="nombre" type="text" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ $temas->nombre }}" required autocomplete="nombre" autofocus>
+                                    @error('nombre')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="contenido" class="col-md-4 col-form-label text-md-right">Contenido</label>
+                                <div class="col-md-6">
+                                    <textarea id="contenido" class="form-control @error('contenido') is-invalid @enderror" name="contenido" value="{{ $temas->contenido }}" rows="4" cols="50" required autocomplete="contenido"></textarea>
+                                    @error('contenido')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="documento_tema" class="col-md-4 col-form-label text-md-right">Documento Tema</label>
+                                <div class="col-md-6">
+                                    <input id="documento_tema" type="file" class="form-control @error('documento_tema') is-invalid @enderror btn-success" name="documento_tema" value="{{ $temas->documento_tema }}" required>
+                                    @if (!empty($temas->documento_tema))
+                                      <span>Archivo actual: </span>
+                                      <img src="/fotos/{{ $temas->documento_tema }}" width="200" class="img-fluid">
+                                    @else
+                                      Aún no se ha cargado el archivo
+                                    @endif
+                                    @error('documento_tema')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-success">Actualizar</button>
+                                    <a href="{{ route('temas') }}" class="btn btn-primary">Cancelar</a>
+                                </div>
+                            </div>
+                          </form>
+                        @else
+                          <form method="POST" action="{{ route('temas/guardar') }}">
+                            @csrf
+                            <input type="hidden" name="_method" value="PUT">
+                            <div class="form-group row">
+                                <label for="nombre" class="col-md-4 col-form-label text-md-right">Nombre</label>
+                                <div class="col-md-6">
+                                    <textarea id="nombre" class="form-control @error('nombre') is-invalid @enderror" name="nombre" value="{{ old('nombre') }}" rows="4" cols="50" required autocomplete="contenido"></textarea>
+                                    @error('nombre')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="contenido" class="col-md-4 col-form-label text-md-right">Contenido</label>
+                                <div class="col-md-6">
+                                    <textarea id="contenido" class="form-control @error('contenido') is-invalid @enderror" name="contenido" value="{{ old('contenido') }}" rows="4" cols="50" required autocomplete="contenido"></textarea>
+                                    @error('contenido')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="documento_tema" class="col-md-4 col-form-label text-md-right">Documento Tema</label>
+                                <div class="col-md-6">
+                                    <input id="documento_tema" type="file" class="form-control @error('documento_tema') is-invalid @enderror" name="documento_tema" value="Subir Tema" required>
+                                    @error('documento_tema')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-success">Añadir</button>
+                                    <a href="{{ route('temas') }}" class="btn btn-primary">Cancelar</a>
+                                </div>
+                            </div>
+                          </form>
+                        @endif
                       @endif
-                      <div class="form-group row mb-0">
-                          <div class="col-md-6 offset-md-4">
-                              <button type="submit" class="btn btn-success">Añadir</button>
-                              <a href="{{ route('temas') }}" class="btn btn-primary">Cancelar</a>
-                          </div>
-                      </div>
                   </div>
               </div>
           </div>
