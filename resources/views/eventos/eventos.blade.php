cb3db23bfd8054d7877728f530b4826891a1c31d
WalterDAW <co2full@outlook.es>
2021-02-24 20:05:18 +0100

    Modificado la vista de eventos

--- resources/views/eventos/eventos.blade.php
+++ resources/views/eventos/eventos.blade.php
@@ -45,7 +45,6 @@
                         </tr>
                         @foreach($eventos as $evento)
                             <tr>
-                                <td class="v-align-middle">{{ $evento->codigo }}</td>
                                 <td class="v-align-middle">{{ $evento->nombre }}</td>
                                 <td class="v-align-middle">{{ $evento->descripcion }}</td>
                                 <td class="v-align-middle">{{ $evento->disponibilidad }}</td>
@@ -53,8 +52,27 @@
                                   <form action="{{ route('eventos/eliminar', $evento->id) }}" method="POST" class="form-horizontal" role="form" onsubmit="return confirmarEliminar()">
                                       <input type="hidden" name="_method" value="PUT">
                                       <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                      <a href="{{ route('eventos/actualizar', $evento->id) }}" class="btn btn-primary">Modificar</a>
-                                      <button type="submit" class="btn btn-danger">Eliminar</button>
+                                      @if(Auth::check())
+                                        @if(Auth::user()->hasRole('admin'))
+                                          <a href="{{ route('eventos/editar', $asistencia->id) }}" class="btn btn-primary">Modificar</a>
+                                          <button type="submit" class="btn btn-danger" data-toggle="modal" data-target="#myModal">Eliminar</button>
+                                          @include('alerts.dialogos')
+                                        @else
+                                            @if(Auth::user()->hasRole('student'))
+                                              <a href="{{ route('eventos/editar', $asistencia->id) }}" class="btn btn-primary" disabled>Modificar</a>
+                                              <button type="submit" class="btn btn-danger" disabled>Eliminar</button>
+                                            @else
+                                              @if(Auth::user()->hasRole('teacher'))
+                                                  <a href="{{ route('eventos/editar', $asistencia->id) }}" class="btn btn-primary">Modificar</a>
+                                                  <button type="submit" class="btn btn-danger">Eliminar</button>
+                                              @else
+                                                  if(Auth::user()->hasRole('user'))
+                                                    <a href="{{ route('eventos/editar', $asistencia->id) }}" class="btn btn-primary" disabled>Modificar</a>
+                                                    <button type="submit" class="btn btn-danger" disabled>Eliminar</button>
+                                                  @endif
+                                            @endif
+                                          @endif
+                                      @endif
                                   </form>
                                 </td>
                             </tr>
