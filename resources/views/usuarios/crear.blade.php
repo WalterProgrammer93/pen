fc3f83021fb41efe338c0059b1161335fdd5674b
WalterDAW <co2full@outlook.es>
2021-02-24 20:06:30 +0100

    Modificado la vista de crear usuarios

--- resources/views/usuarios/crear.blade.php
+++ resources/views/usuarios/crear.blade.php
@@ -15,10 +15,14 @@
                 <div class="card-header">Crear usuario</div>
                 <div class="card-body">
                     <!-- Obtengo la sesión actual del usuario -->
-                    {{ $message=Session::get('message') }}
+                    @if (Session::has('message'))
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('message') }}
+                        </div>
+                    @endif
                     <!-- Muestro el mensaje de validación -->
                     @include('alerts.request')
-                    <form method="POST" action="{{ route('usuarios/store') }}">
+                    <form method="POST" action="{{ route('usuarios/guardar') }}">
                         <input type="hidden" name="_method" value="PUT">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         @include('usuarios.index')
