6d4a6907546b1894dfdc7ab7d62598d3e43e73ba
WalterDAW <co2full@outlook.es>
2021-03-22 12:27:17 +0100

    Modificado el metodo edit del controlador de asignaturas

--- app/Http/Controllers/AsignaturaController.php
+++ app/Http/Controllers/AsignaturaController.php
@@ -72,10 +72,10 @@ class AsignaturaController extends Controller
     {
         $asignaturas = Asignatura::find($id);
         $cursos = Curso::find($id);
-        $aulas = Aula::find($id);
         $cursos = Curso::orderBy('id')->pluck('nombre', 'id')->toArray();
+        $aulas = Aula::find($id);
         $aulas = Aula::orderBy('id')->pluck('etiqueta', 'id')->toArray();
-        return view("asignaturas.editar", ['asignaturas' => $asignaturas], ['cursos' => $cursos], ['aulas' => $aulas]);
+      return view("asignaturas.editar", ['asignaturas' => $asignaturas], ['cursos' => $cursos], ['aulas' => $aulas]);
     }
 
     /**
