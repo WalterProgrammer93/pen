89c7e0344a2bd6025fb3f25c645bc8347f8cb751
Walter2021-debug <proyectoDAM2.2021@gmail.com>
2021-05-10 12:22:57 +0200

    Crear migracion y tabla roles de la relacion de la tabla perfils y users

--- database/migrations/2021_05_10_101935_create_roles_table.php
+++ database/migrations/2021_05_10_101935_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePerfilUserTable extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreatePerfilUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('perfil_user', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('perfil_id')->unsigned();
             $table->integer('user_id')->unsigned();
-            $table->foreign('perfil_id')->references('id')->on('perfils');
+            $table->integer('perfil_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('perfil_id')->references('id')->on('perfils');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreatePerfilUserTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('perfil_user');
+        Schema::dropIfExists('roles');
     }
 }
