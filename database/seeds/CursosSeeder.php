3b83d3f8629d865fe91a4c5537cbda9b05318888
Walter2021-debug <proyectoDAM2.2021@gmail.com>
2021-06-15 10:07:04 +0200

    Creando los seeder o semillas para la insercion de datos para agilizar el proceso de prueba de la aplicacion

--- database/seeds/CursosSeeder.php
+++ database/seeds/CursosSeeder.php
@@ -11,6 +11,74 @@ class CursosSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $curso = new Curso();
+        $curso->nombre = 'ESO1';
+        $curso->descripcion = 'Primero de la educación secundaria obligatoria';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'ESO2';
+        $curso->descripcion = 'Segundo de la educación secundaria obligatoria';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'ESO3';
+        $curso->descripcion = 'Tercero de la educación secundaria obligatoria';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'ESO4';
+        $curso->descripcion = 'Cuarto de la educación secundaria obligatoria';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'Bachiller1';
+        $curso->descripcion = 'Primero de bachillerato';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'Bachiller2';
+        $curso->descripcion = 'Segundo de bachillerato';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'ESO1';
+        $curso->descripcion = 'Primero de la educación secundaria obligatoria';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'ESO1';
+        $curso->descripcion = 'Primero de la educación secundaria obligatoria';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'SMR1';
+        $curso->descripcion = 'Primero de sistemas microinformáticos y redes';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'SMR2';
+        $curso->descripcion = 'Segundo de sistemas microinformáticos y redes';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'DAW1';
+        $curso->descripcion = 'Primero de desarrollo de aplicaciones web';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'DAW2';
+        $curso->descripcion = 'Segundo de desarrollo de aplicaciones web';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'DAM1';
+        $curso->descripcion = 'Primero de desarrollo de aplicaciones multiplataforma';
+        $curso->save();
+
+        $curso = new Curso();
+        $curso->nombre = 'DAM2';
+        $curso->descripcion = 'Segundo de desarrollo de aplicaciones multiplataforma';
+        $curso->save();
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,20 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call(PerfilsTableSeeder::class);    // Los usuarios necesitarán los roles previamente generados
+      $this->call(UsersSeeder::class);
+      $this->call(CursosSeeder::class);
+      $this->call(AlumnosSeeder::class);
+      $this->call(AulasSeeder::class);
+      $this->call(AsignaturasSeeder::class);
+      $this->call(DepartamentosSeeder::class);
+      $this->call(ProfesorsSeeder::class);
+      $this->call(NotasSeeder::class);
+      $this->call(ClaseSeeder::class);
+      $this->call(AsistenciasSeeder::class);
+      $this->call(EventosSeeder::class);
+      $this->call(ReservasSeeder::class);
+      $this->call(TemasSeeder::class);
+      $this->call(TareasSeeder::class);
+      $this->call(RoleSeeder::class);
     }
 }
