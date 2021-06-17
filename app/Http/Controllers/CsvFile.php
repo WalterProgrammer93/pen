ab0a32f06e0bd2dcbb3a4aa5a7e25f1c822dfe6f
Walter2021-debug <proyectoDAM2.2021@gmail.com>
2021-06-17 02:29:10 +0200

    Modificado el controlador de CsvFile

--- app/Http/Controllers/CsvFile.php
+++ app/Http/Controllers/CsvFile.php
@@ -84,10 +84,186 @@ class CsvFile extends Controller
 
     public function ExportAula()
     {
-        return Excel::download(new CsvExport, 'alumnos.csv');
+        return Excel::download(new CsvExport, 'aulas.csv');
     }
 
     public function ImportAula()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexClase()
+    {
+        return view('clases.importar');
+    }
+
+    public function ExportClase()
+    {
+        return Excel::download(new CsvExport, 'clases.csv');
+    }
+
+    public function ImportClase()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexCurso()
+    {
+        return view('cursos.importar');
+    }
+
+    public function ExportCurso()
+    {
+        return Excel::download(new CsvExport, 'cursos.csv');
+    }
+
+    public function ImportCurso()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexDepartamento()
+    {
+        return view('departamentos.importar');
+    }
+
+    public function ExportDepartamento()
+    {
+        return Excel::download(new CsvExport, 'departamentos.csv');
+    }
+
+    public function ImportDepartamento()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexEvento()
+    {
+        return view('eventos.importar');
+    }
+
+    public function ExportEvento()
+    {
+        return Excel::download(new CsvExport, 'eventos.csv');
+    }
+
+    public function ImportEvento()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexNota()
+    {
+        return view('notas.importar');
+    }
+
+    public function ExportNota()
+    {
+        return Excel::download(new CsvExport, 'notas.csv');
+    }
+
+    public function ImportNota()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexPerfil()
+    {
+        return view('perfiles.importar');
+    }
+
+    public function ExportPerfil()
+    {
+        return Excel::download(new CsvExport, 'perfiles.csv');
+    }
+
+    public function ImportPerfil()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexProfesor()
+    {
+        return view('profesores.importar');
+    }
+
+    public function ExportProfesor()
+    {
+        return Excel::download(new CsvExport, 'profesores.csv');
+    }
+
+    public function ImportProfesor()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexReserva()
+    {
+        return view('reservas.importar');
+    }
+
+    public function ExportReserva()
+    {
+        return Excel::download(new CsvExport, 'reservas.csv');
+    }
+
+    public function ImportReserva()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexRol()
+    {
+        return view('roles.importar');
+    }
+
+    public function ExportRol()
+    {
+        return Excel::download(new CsvExport, 'roles.csv');
+    }
+
+    public function ImportRol()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexTarea()
+    {
+        return view('tareas.importar');
+    }
+
+    public function ExportTarea()
+    {
+        return Excel::download(new CsvExport, 'tareas.csv');
+    }
+
+    public function ImportTarea()
+    {
+        Excel::import(new CsvImport, request()->file('file'));
+        return back();
+    }
+
+    public function indexTema()
+    {
+        return view('temas.importar');
+    }
+
+    public function ExportTema()
+    {
+        return Excel::download(new CsvExport, 'temas.csv');
+    }
+
+    public function ImportTema()
     {
         Excel::import(new CsvImport, request()->file('file'));
         return back();
